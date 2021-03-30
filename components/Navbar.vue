64a28b4fa9da7f5a65adeb27bb4f1cbc27a34700
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-03-30 17:12:40 +0200

    basic SearchBar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,28 @@
 <template>
-  <nav class="w-full bg-gray-100 text-blue-700">
-    <nuxt-link to="/">
-      home
-    </nuxt-link>
-    <nuxt-link to="/login">
-      login
-    </nuxt-link>
-    <nuxt-link to="/r/vue">
-      r/vue
-    </nuxt-link>
-    <nuxt-link to="/r/react">
-      r/react
-    </nuxt-link>
-  </nav>
+  <div>
+    <nav class="w-full bg-white text-blue-700">
+      <nuxt-link to="/">
+        home
+      </nuxt-link>
+      <nuxt-link to="/login">
+        login
+      </nuxt-link>
+      <nuxt-link to="/r/vue">
+        r/vue
+      </nuxt-link>
+      <nuxt-link to="/r/react">
+        r/react
+      </nuxt-link>
+    </nav>
+    <SearchBar />
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SearchBar from '@/components/SearchBar.vue'
+
+export default Vue.extend({
+  components: { SearchBar }
+})
+</script>
---
+++ components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cursor-text bg-gray-100 px-4 py-2 w-80 flex items-center hover:bg-white group " @click="focusInput">
+    <SearchIcon class="text-gray-500" />
+    <input ref="search" type="text" placeholder="Search" class="group-hover:bg-white outline-none px-2 bg-gray-100">
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import SearchIcon from '@/components/svg/SearchIcon.vue';
+export default Vue.extend({
+  components: { SearchIcon },
+  methods: {
+    focusInput () {
+      this.$refs.search.focus()
+    }
+  }
+})
+</script>
