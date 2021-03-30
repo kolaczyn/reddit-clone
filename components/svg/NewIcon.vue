8381599d20f29a905eb094eb34d4ed00aa2de9bb
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-03-30 18:48:35 +0200

    add icons

---
+++ components/svg/NewIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="black"
+    width="18px"
+    height="18px"
+    class="fill-current"
+  >
+    <g><rect fill="none" height="24" width="24" /></g>
+    <g>
+      <path
+        d="M17.25 9L15.42 6.9075L15.675 4.14L12.9675 3.525L11.55 1.125L9 2.22L6.45 1.125L5.0325 3.5175L2.325 4.125L2.58 6.9L0.75 9L2.58 11.0925L2.325 13.8675L5.0325 14.4825L6.45 16.875L9 15.7725L11.55 16.8675L12.9675 14.475L15.675 13.86L15.42 11.0925L17.25 9Z"
+      />
+    </g>
+  </svg>
+</template>
