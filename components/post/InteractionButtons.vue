26e417b9d6bf1f1340d887c27aa9cbb219a10bd7
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-03-31 11:20:47 +0200

    create <InteractionButtons />

--- components/post/InteractionButtons.vue
+++ components/post/InteractionButtons.vue
@@ -1,5 +1,35 @@
 <template>
-  <section>
-    leave a like and subscirbe
-  </section>
+  <ul class="flex">
+    <li>
+      <PostInteractionButton>
+        <template #icon>
+          <IconComment />
+        </template>
+        3 Comments
+      </PostInteractionButton>
+    </li>
+    <li>
+      <PostInteractionButton>
+        <template #icon>
+          <IconShare />
+        </template>
+        Share
+      </PostInteractionButton>
+    </li>
+    <li>
+      <PostInteractionButton>
+        <template #icon>
+          <IconSave />
+        </template>
+        Save
+      </PostInteractionButton>
+    </li>
+    <li>
+      <PostInteractionButton>
+        <template #icon>
+          <IconTripleDot />
+        </template>
+      </PostInteractionButton>
+    </li>
+  </ul>
 </template>
--- components/post/index.vue
+++ components/post/index.vue
@@ -3,11 +3,11 @@
     <PostUpvoteSection class="bg-gray-200 p-2 w-20">
       29.5k
     </PostUpvoteSection>
-    <div class="p-2">
+    <div class="p-2 pb-0">
       <PostMetaInfo user="ser" creation-date="11 hours ago" class="mb-2" />
       <PostTitle />
       <PostContent />
-      <PostInteractionButtons />
+      <PostInteractionButtons class="mt-2" />
     </div>
   </BasePaper>
 </template>
