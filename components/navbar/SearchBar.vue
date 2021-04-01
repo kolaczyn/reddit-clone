d6709015fb4a1cafc2164675c6b0b7260ac6d70c
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-04-01 14:24:59 +0200

    fix <SearchBar/>'s background color Now it behaves exactly like Reddit's search bar: it looks focused if the bar is focused, it doesn't look focused otherwise, even if there's text in the input

--- components/navbar/SearchBar.vue
+++ components/navbar/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div
-      class="cursor-text bg-gray-100 px-4 py-2 w-80 flex items-center hover:bg-white group "
+      :class="['cursor-text px-4 py-2 w-80 flex items-center hover:bg-white group border hover:border-blue-400 rounded', focus ? 'bg-white' : 'bg-gray-100', focus && 'border-blue-400']"
       @click="focusInput"
     >
       <IconSearch class="text-gray-500" />
@@ -10,7 +10,9 @@
         v-model="inputText"
         type="text"
         placeholder="Search"
-        class="group-hover:bg-white outline-none px-2 bg-gray-100 flex-1"
+        :class="['group-hover:bg-white outline-none px-2 flex-1', focus ? 'bg-white' : 'bg-gray-100']"
+        @focus="focus=true"
+        @blur="focus=false"
       >
       <button v-if="!!inputText" class="focus:outline-none p-0.5 text-gray-500 hover:text-blue-400 focus:text-blue-500" @click="clearInput">
         <IconClose class="cursor-pointer" />
@@ -24,7 +26,9 @@ import Vue from 'vue'
 export default Vue.extend({
   data () {
     return {
-      inputText: ''
+      inputText: '',
+      // I keep track if the input if focus, so I can style the component appropriately
+      focus: false
     }
   },
   methods: {
