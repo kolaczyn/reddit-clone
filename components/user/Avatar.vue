32d521aed6a6109542fbbd019dec8eaa2695228a
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-04-05 22:40:17 +0200

    create <UserAvatar /> with background

---
+++ components/user/Avatar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-24 border-white border-4 rounded">
+    <img :src="require(`~/assets/img/${imgName}`)">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgName: {
+      type: String,
+      required: false,
+      default: 'default-subreddit-image.png'
+    }
+  }
+}
+</script>
--- components/user/Card.vue
+++ components/user/Card.vue
@@ -1,25 +1,30 @@
 <template>
-  <BasePaper class="p-2">
-    <div class="text-sm font-bold">
-      u/{{ username }}
+  <BasePaper>
+    <div class="bg-blue-500 h-24 relative mb-8">
+      <UserAvatar class="absolute left-2 top-8" />
     </div>
-    <p>{{ description }}</p>
-    <section class="grid grid-cols-2 gap-2">
-      <section class="col-span-1">
-        <span class="font-bold">Karma</span>
-        <span>{{ karma }}</span>
+    <div class="p-2">
+      <div class="text-sm font-bold">
+        u/{{ username }}
+      </div>
+      <p>{{ description }}</p>
+      <section class="grid grid-cols-2 gap-2">
+        <section class="col-span-1">
+          <span class="font-bold">Karma</span>
+          <span>{{ karma }}</span>
+        </section>
+        <section class="col-span-1">
+          <span class="font-bold">Cake day</span>
+          <span>{{ cakeDay }}</span>
+        </section>
       </section>
-      <section class="col-span-1">
-        <span class="font-bold">Cake day</span>
-        <span>{{ cakeDay }}</span>
-      </section>
-    </section>
-    <BaseButton class="w-full">
-      Follow
-    </BaseButton>
-    <button class="font-bold text-blue-600 ml-auto block">
-      More options
-    </button>
+      <BaseButton class="w-full">
+        Follow
+      </BaseButton>
+      <button class="font-bold text-blue-600 ml-auto block">
+        More options
+      </button>
+    </div>
   </BasePaper>
 </template>
 
