05e2d8c855a738ec2e674fd66f7ff37fb0b168f4
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-04-05 22:29:00 +0200

    create <UserCard />

---
+++ components/user/Card.vue
@@ -0,0 +1,47 @@
+<template>
+  <BasePaper class="p-2">
+    <div class="text-sm font-bold">
+      u/{{ username }}
+    </div>
+    <p>{{ description }}</p>
+    <section class="grid grid-cols-2 gap-2">
+      <section class="col-span-1">
+        <span class="font-bold">Karma</span>
+        <span>{{ karma }}</span>
+      </section>
+      <section class="col-span-1">
+        <span class="font-bold">Cake day</span>
+        <span>{{ cakeDay }}</span>
+      </section>
+    </section>
+    <BaseButton class="w-full">
+      Follow
+    </BaseButton>
+    <button class="font-bold text-blue-600 ml-auto block">
+      More options
+    </button>
+  </BasePaper>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    karma: {
+      type: Number,
+      required: true
+    },
+    cakeDay: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
