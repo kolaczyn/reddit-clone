278b17100f668f00672505d4db5bf69690345b28
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-04-06 14:53:10 +0200

    move subreddit code from / to /r/_subreddit

--- pages/r/_subreddit.vue
+++ pages/r/_subreddit.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <p>Welcome on r/{{ subreddit }}</p>
+    <SubredditHeader subreddit-name="r/dadjokes">
+      r/DadJokes - the best Dad Jokes on reddit
+    </SubredditHeader>
+    <LayoutMediumContainer>
+      <LayoutMainTwoCols class="mt-4">
+        <template #first>
+          <FilterBar class="mb-2" />
+          <PostsTimeline />
+        </template>
+        <template #second>
+          <Sidebar />
+        </template>
+      </LayoutMainTwoCols>
+    </LayoutMediumContainer>
   </div>
 </template>
 
