dc53e5ad048cc6c4c25f266b032f0b48a86de473
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-04-06 14:41:44 +0200

    move subreddit header, fix header's margin

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="min-h-screen bg-gray-800">
     <Navbar />
-    <SubredditHeader subreddit-name="r/dadjokes">
-      r/DadJokes - the best Dad Jokes on reddit
-    </SubredditHeader>
     <Nuxt />
   </div>
 </template>
